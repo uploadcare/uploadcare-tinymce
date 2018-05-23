e18072ea83ad46e0fdcb67963037c5f771855f3c
Zarema <zarema.khalilova@gmail.com>
2018-05-23 15:46:11 +0300

    Fix path to plugin in demo

--- scripts/browser-sync.config.js
+++ scripts/browser-sync.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   files: ['index.html', 'dist', 'demo'],
   server: './',
+  rewriteRules: [
+    {
+      match: 'http://uploadcare.github.io/uploadcare-tinymce/dist/',
+      replace: '/../dist/',
+    },
+  ],
 }
