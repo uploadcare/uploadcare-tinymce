4f4e614101b416fb3b3d016b8914d966cadc44c5
Александр Гренишин <nd0ut.me@gmail.com>
2019-06-11 17:52:53 +0300

    update to tinymce 5

--- src/uploadcare-tinymce.js
+++ src/uploadcare-tinymce.js
@@ -1,39 +1,38 @@
+import icon from './icons/uploadcare.svg'
+
 var uploadcareDefaultOptions = {
   integration: getIntegration(),
   crop: '',
 }
 
 tinymce.create('tinymce.plugins.UploadcarePlugin', {
-  init: function(ed, url) {
+  init: function(editor) {
     tinymce.ScriptLoader.add('https://ucarecdn.com/widget/' + '$_WIDGET_VERSION' + '/uploadcare/uploadcare.full.min.js')
     tinymce.ScriptLoader.loadQueue()
 
-    var uploadcareOptions = Object.keys(ed.settings)
+    editor.ui.registry.addIcon('uploadcare', icon)
+
+    var uploadcareOptions = Object.keys(editor.settings)
       .filter(function(settingName) {
         return settingName.search('^uploadcare_') !== -1
       })
       .reduce(function(options, settingName) {
         var optionName = settingName
           .replace('uploadcare_', '')
-          .replace(/(_[a-z])/g, function(v) { return v.toUpperCase() })
+          .replace(/(_[a-z])/g, function(v) {
+            return v.toUpperCase()
+          })
           .replace('_', '')
 
-        options[optionName] = ed.settings[settingName]
+        options[optionName] = editor.settings[settingName]
 
         return options
       }, uploadcareDefaultOptions)
 
-    ed.addButton('uploadcare', {
-      title: 'Uploadcare',
-      cmd: 'showUploadcareDialog',
-      image: url + '/icons/uploadcare.png',
-      stateSelector: 'img',
-    })
-
-    ed.addCommand('showUploadcareDialog', function() {
+    function showUploadcareDialog() {
       var cdnBase = uploadcareOptions.cdnBase || uploadcare.defaults.cdnBase
       var file = null
-      var selectedNode = ed.selection.getNode()
+      var selectedNode = editor.selection.getNode()
 
       if (selectedNode.nodeName === 'IMG') {
         if (selectedNode.src.indexOf(cdnBase) === 0) {
@@ -49,7 +48,7 @@ tinymce.create('tinymce.plugins.UploadcarePlugin', {
       uploadcare.openDialog(file, uploadcareOptions).done(function(file) {
         file.done(function(fileInfo) {
           if (fileInfo.isImage) {
-            ed.selection.setNode(tinymce.activeEditor.dom.create('img', {src: fileInfo.cdnUrl}))
+            editor.selection.setNode(tinymce.activeEditor.dom.create('img', {src: fileInfo.cdnUrl}))
           }
           else if (selectedNode.nodeName === 'A') {
             selectedNode.parentNode.replaceChild(
@@ -58,10 +57,15 @@ tinymce.create('tinymce.plugins.UploadcarePlugin', {
             )
           }
           else {
-            ed.selection.setNode(tinymce.activeEditor.dom.create('a', {href: fileInfo.cdnUrl}, fileInfo.name))
+            editor.selection.setNode(tinymce.activeEditor.dom.create('a', {href: fileInfo.cdnUrl}, fileInfo.name))
           }
         })
       })
+    }
+
+    editor.ui.registry.addButton('uploadcare', {
+      onAction: showUploadcareDialog,
+      icon: 'uploadcare',
     })
   },
 
